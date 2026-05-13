c37b813e54604fa5c8a878521803e46e046839ab
mariam <mariamshahathamada@gmail.com>
2026-05-14 01:23:47 +0300

    C2

--- libraryservice/src/main/java/com/ghada/library/libraryModel/BorrowStatus.java
+++ libraryservice/src/main/java/com/ghada/library/libraryModel/BorrowStatus.java
@@ -3,4 +3,3 @@ package com.ghada.library.libraryModel;
 public enum BorrowStatus {
  BORROWED,RETURNED, LATE, VERY_LATE
 }
-// BorrowStatus is an enum that represents the different statuses of a borrowed item in the library. It includes the following values:
